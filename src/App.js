60e8faff05c200f882f4c909a813fe46b11e0200
youngleek0918 <youngleek0918@gmail.com>
2021-02-20 00:02:35 +0900

    created header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <h1>Slack</h1>
+      {/* Header */}
+      <Header />
+      {/* Sidebar */}
+      {/* React-router => chat screen */}
     </div>
   );
 }
